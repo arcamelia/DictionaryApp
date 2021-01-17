ef098be37723112dc080979bcd9eba3ca43e3739
aarci <aarci@students.cs.ubc.ca>
2021-01-17 01:12:54 -0500

    emoji!!

--- src/ui/DictionaryApp.java
+++ src/ui/DictionaryApp.java
@@ -11,7 +11,7 @@ public class DictionaryApp extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("dictionary.fxml"));
-        stage.setTitle("Dictionary");
+        stage.setTitle("Word. ✌🏼 Dictionary");
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
     }
